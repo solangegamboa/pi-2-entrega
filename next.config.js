3e90212169da51257cc02a707e9a9412f3e287e3
Sol Gamboa <3474054+solangegamboa@users.noreply.github.com>
2023-05-01 19:04:15 -0300

    deploy

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
@@ -5,4 +17,12 @@ const nextConfig = {
     },
 };
 
-module.exports = nextConfig;
+module.exports = {
+    nextConfig: nextConfig,
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+    images: {
+        loader: 'imgix',
+        path: 'the "domain" of your Imigix source',
+    }
+  }
